d64def44a28a8d1ac0885cd4a570eba34ffa5390
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-08-13 11:42:03 +0200

    Updated js file with Pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-alert('Hello world');
 
-let favoriteFood = 'Rice'
-document.write(favoriteFood);
+// list of pokemon
+let pokemonList = [
+  { name: 'Venusaur', height: 1, type: ['grass', 'poison']},
+  { name: 'Charizard', height: 2, type: ['fire', 'flying']},
+  { name: 'Beedrill', height: 1, type: ['bug', 'poison']},
+  { name: 'Pidgeot', height: 3, type: ['flying', 'normal']},
+  { name: 'Nidoking', height: 1, type: ['ground', 'poison']}
+]
