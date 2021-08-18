7485bc55d589e0c63361ff634361da5f2bc21f3e
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-08-18 16:31:16 +0200

    added a loop to list pokemon names with height

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,43 @@
 
 // list of pokemon
 let pokemonList = [
-  { name: 'Venusaur', height: 1, type: ['grass', 'poison']},
-  { name: 'Charizard', height: 2, type: ['fire', 'flying']},
-  { name: 'Beedrill', height: 1, type: ['bug', 'poison']},
-  { name: 'Pidgeot', height: 3, type: ['flying', 'normal']},
-  { name: 'Nidoking', height: 1, type: ['ground', 'poison']}
+  {
+    name: 'Venusaur',
+    height: 1,
+    type: ['grass', 'poison']
+  },
+
+  {
+    name: 'Charizard',
+    height: 2,
+    type: ['fire', 'flying']
+  },
+
+  {
+    name: 'Beedrill',
+    height: 1,
+    type: ['bug', 'poison']
+  },
+
+  {
+    name: 'Pidgeot',
+    height: 3,
+    type: ['flying', 'normal']
+  },
+
+  {
+    name: 'Nidoking',
+    height: 1,
+    type: ['ground', 'poison']
+  }
 ]
+
+// a loop which lists each pokemon name with thier respective height
+// gives a special lebel when height is more than 2
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 2) {
+    document.write('<p>' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ')' + ' -Wow that is big! </p>'  );
+    } else {
+    document.write('<p>' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ')</p>');
+  }
+}
