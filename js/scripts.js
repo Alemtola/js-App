1668812aa7dbb2f259573354b391c59806ef4697
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-08-24 18:33:42 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,62 @@
 
 // list of pokemon
-let pokemonList = [
-  {
-    name: 'Venusaur',
-    height: 1,
-    type: ['grass', 'poison']
-  },
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Venusaur',
+      height: 1,
+      type: ['grass', 'poison']
+    },
 
-  {
-    name: 'Charizard',
-    height: 2,
-    type: ['fire', 'flying']
-  },
+    {
+      name: 'Charizard',
+      height: 2,
+      type: ['fire', 'flying']
+    },
 
-  {
-    name: 'Beedrill',
-    height: 1,
-    type: ['bug', 'poison']
-  },
+    {
+      name: 'Beedrill',
+      height: 1,
+      type: ['bug', 'poison']
+    },
 
-  {
-    name: 'Pidgeot',
-    height: 3,
-    type: ['flying', 'normal']
-  },
+    {
+      name: 'Pidgeot',
+      height: 3,
+      type: ['flying', 'normal']
+    },
 
-  {
-    name: 'Nidoking',
-    height: 1,
-    type: ['ground', 'poison']
+    {
+      name: 'Nidoking',
+      height: 1,
+      type: ['ground', 'poison']
+    }
+  ];
+
+  function add(newPokemon) {
+    pokemonList.push(newPokemon);
   }
-]
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+}) ();
+
+pokemonRepository.add (
+  {
+    name:'Metapod',
+    height: 0.7
+  }
+);
 
 // a loop which lists each pokemon name with thier respective height
 // gives a special lebel when height is greater than 2
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.forEach(function(pokemon) {
   if (pokemon.height > 2) {
     document.write('<p>' + pokemon.name + ' (height:' + pokemon.height + ')' + ' -Wow that is big! </p>'  );
     } else {
